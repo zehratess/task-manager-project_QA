7162eb821c597e6b2958c9899de407b74c2f1502
Nisanur Şen <230229075@kocaeli.edu.tr>
2025-11-08 22:08:33 +0300

    7

--- frontend/task-manager/src/components/Charts/CustomTooltip.jsx
+++ frontend/task-manager/src/components/Charts/CustomTooltip.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const CustomTooltip = ({ active, payload }) => {
   if (active && payload && payload.length) {
     return (
-      <div className="bg-white">
-        <p className="">{payload[0].name}</p>
-        <p className="">
-          Count: <span className="">{payload[0].value}</span>
+      <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300">
+        <p className="text-xs font-semibold text-purple-800 mb-1">
+          {payload[0].name}
+        </p>
+        <p className="text-sm text-gray-600">
+          Count:{" "}
+          <span className="text-sm font-medium text-gray-900">
+            {payload[0].value}
+          </span>
         </p>
       </div>
     );
@@ -15,4 +20,4 @@ const CustomTooltip = ({ active, payload }) => {
   return null;
 };
 
-export default CustomTooltip
+export default CustomTooltip;
