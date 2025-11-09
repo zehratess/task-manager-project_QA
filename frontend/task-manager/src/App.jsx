d68b81217494ea3ec2154493c6c9846ba4bdb6db
Zehra Ateş <zehrates07@gmail.com>
2025-11-09 16:17:03 +0300

    23.35 app.jsx ve routes

--- frontend/task-manager/src/App.jsx
+++ frontend/task-manager/src/App.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+
+import Dashboard from './pages/Admin/Dashboard';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import ManageTasks from './pages/Admin/ManageTasks';
+import CreateTask from './pages/Admin/CreateTask';
+import ManageUsers from './pages/Admin/ManageUsers';
+
+import UserDashboard from './pages/User/UserDashboard';
+import MyTasks from './pages/User/MyTasks';
+import ViewTaskDetails from './pages/User/ViewTaskDetails';
+
+import PrivateRoute from './routes/PrivateRoute';
 
 const App = () => {
-  return <div> App</div>;
+  return (
+    <div>
+      <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<SignUp />} />
+
+            {/*Admin Routes*/}
+            <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+              <Route path="/admin/dashboard" element={<Dashboard />} />
+              <Route path="/admin/tasks" element={<ManageTasks />} />
+              <Route path="/admin/create-task" element={<CreateTask />} />
+              <Route path="/admin/users" element={<ManageUsers />} />
+            </Route>
+
+            {/*User Routes*/}
+            <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+              <Route path="/user/dashboard" element={<UserDashboard />} />
+              <Route path="/user/tasks" element={<MyTasks />} />
+              <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
+            </Route>
+          </Routes>
+      </Router>
+    </div>
+  )
 };
 
 export default App;
---
+++ frontend/task-manager/src/routes/PrivateRoute.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PrivateRoute = ({allowedRoles}) => {
+    return (
+    <Outlet />
+    )
+};
+
+export default PrivateRoute;
