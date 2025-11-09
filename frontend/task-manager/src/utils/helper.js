6372bbd7ab4507da7001d58c3a7a69a0ff32cd9b
Zehra Ateş <zehrates07@gmail.com>
2025-11-09 18:28:43 +0300

    2h44s

--- frontend/task-manager/src/utils/helper.js
+++ frontend/task-manager/src/utils/helper.js
@@ -2,3 +2,12 @@ export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 };
+
+export const addThousandSeparators = (num) => {
+    if(num) == null || isNaN(num)) return '';
+
+    const [integerPart, fractionalPart] = num.toString().split('.');
+    const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    return fractionalPart ? `${formattedInteger}.${fractionalPart}` : formattedInteger;
+}; 
