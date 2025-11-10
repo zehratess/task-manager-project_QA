e1fb3c2f9b034f37a5893dacb4e4e9982ab2713b
Zehra Ateş <zehrates07@gmail.com>
2025-11-10 12:15:14 +0300

    of

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+//Middleware
+app.use(express.json());
+
+// ROUTES
+
+//Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
