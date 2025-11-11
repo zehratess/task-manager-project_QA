fdd578d8f4000de7bbdfe0032b6606eb2cebadfd
Zehra Ateş <zehrates07@gmail.com>
2025-11-11 22:56:49 +0300

    login resmi

--- frontend/task-manager/src/components/layouts/AuthLayout.jsx
+++ frontend/task-manager/src/components/layouts/AuthLayout.jsx
@@ -8,8 +8,8 @@ const AuthLayout = ({children})=> {
             {children}
         </div>
         
-    <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 bg-[url('/bg-img.png')] bg-cover bg-no-repeat bg-center overflow-hidden p-8">
-    <img src={UI_IMG} className="w-64 lg:w-[90%]"></img>
+    <div className="hidden md:flex w-[40vw] h-screen bg-blue-50 bg-[url('/bg-img.png')] bg-cover bg-no-repeat bg-center overflow-hidden">
+    <img src={UI_IMG} className="w-full h-full object-cover"></img>
     </div>
     </div>
 };
