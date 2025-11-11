5cef517b1d4d61fd74ffe4820faa1e72771e3f91
Zehra Ateş <zehrates07@gmail.com>
2025-11-11 22:33:18 +0300

    goinon yazısı düzeltildi

--- frontend/task-manager/src/components/layouts/Navbar.jsx
+++ frontend/task-manager/src/components/layouts/Navbar.jsx
@@ -1,35 +1,38 @@
-import React, {useState} from "react";
-import {HiOutlineMenu, HiOutlineX} from "react-icons/hi";
+import React, { useState } from "react";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 import SideMenu from "./SideMenu";
 
-const Navbar = ({activeMenu}) => {
-    const [openSideMenu,setOpenSideMenu] = useState(false);
+const Navbar = ({ activeMenu }) => {
+    const [openSideMenu, setOpenSideMenu] = useState(false);
 
-  return (
-    <div className="flex gap-5 bg-white border boredr-b border-gray-200/50 backdrop-blur-[2px] py-4 pk-7 sticky top-0 z-30">
-        <button 
-        className="block lg:hidden text-black"
-        onClick={() => {
-            setOpenSideMenu(!openSideMenu);
-        }}
-    >
-        {openSideMenu ? (
-            <HiOutlineX className="text-2xl"/>
-        ) : (
-            <HiOutlineMenu className="text-2xl"/>
-        )}
-    </button>
-   
-   <h2 className="text-lg font-medium text-black">GOINON</h2>
+    return (
+        <div className="flex gap-5 bg-white border boredr-b border-gray-200/50 backdrop-blur-[2px] py-4 pk-7 sticky top-0 z-30">
+            <button
+                className="block lg:hidden text-black"
+                onClick={() => {
+                    setOpenSideMenu(!openSideMenu);
+                }}
+            >
+                {openSideMenu ? (
+                    <HiOutlineX className="text-2xl" />
+                ) : (
+                    <HiOutlineMenu className="text-2xl" />
+                )}
+            </button>
 
-   {openSideMenu && (
-    <div className="fixed top-[61px] -ml-4 bg-white">
-        <SideMenu activeMenu={activeMenu} />
-    </div>
-  )
-}
-</div>
-  )
+
+            <header className="flex items-center justify-center h-full w-full">
+                <h2 className="text-xl font-medium text-black">GOINON</h2>
+            </header>
+
+            {openSideMenu && (
+                <div className="fixed top-[61px] -ml-4 bg-white">
+                    <SideMenu activeMenu={activeMenu} />
+                </div>
+            )
+            }
+        </div>
+    )
 }
 
 export default Navbar
