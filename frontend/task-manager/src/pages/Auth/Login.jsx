98851ec6218210cc3a6995d744fd2f9542c71894
Zehra Ateş <zehrates07@gmail.com>
2025-11-11 23:00:37 +0300

    welcome

--- frontend/task-manager/src/pages/Auth/Login.jsx
+++ frontend/task-manager/src/pages/Auth/Login.jsx
@@ -64,7 +64,7 @@ const Login = () => {
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <h3 className="text-xl font-semibold text-black">Welcome!</h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Please enter your details to log in
         </p>
