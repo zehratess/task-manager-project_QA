23a09cb17a1399a61572bd9799673b38cfa3222f
rukiyeberna <rb.turan@hotmail.com>
2026-05-17 14:36:58 +0300

    refactor: simplify PrivateRoute component by removing allowedRoles prop

--- frontend/task-manager/src/routes/PrivateRoute.jsx
+++ frontend/task-manager/src/routes/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-const PrivateRoute = ({allowedRoles}) => {
+const PrivateRoute = () => {
     return (
     <Outlet/>
     )
